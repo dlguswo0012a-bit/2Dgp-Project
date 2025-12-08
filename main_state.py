b48499d3b019d5297d17bec408f526119117a83e
lee hyeonjae <dlguswo0012a@gmail.com>
2025-12-08 19:43:12 +0900

    소리조정

--- main_state.py
+++ main_state.py
@@ -5,7 +5,7 @@ import title_mode as start_mode
 
 open_canvas(1200, 600)
 bgm = load_music('bgm.mp3')
-bgm.set_volume(40)
+bgm.set_volume(30)
 bgm.repeat_play()
 game_framework.run(start_mode)
 close_canvas()
